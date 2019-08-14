59800ba6d99d74ffec901dbd7bb31ced886889ac
William Weisberger <bill.weisberger@gmail.com>
2019-08-13 21:15:36 -0600

    finished the thing

--- capitals.py
+++ capitals.py
@@ -151,3 +151,65 @@ states = [
     "name": "Wyoming",
     "capital": "Cheyenne"
 }]
+
+# add these keys to each dictionary
+for obj in states:
+    obj['correct'] = 0
+    obj['wrong'] = 0
+
+# print(states)
+import random
+
+def copy_states():
+    new_states = states
+    return new_states
+
+def get_state_obj(index):
+    return states[index]
+
+def get_input(obj):
+    guess = input("What is the capital of {}?  ".format(obj['name']))
+    return guess
+
+def check_answer(obj, answer):
+    if obj['capital'].lower() == answer.lower():
+        print('Correct!')
+        obj['correct'] += 1
+    else:
+        obj['wrong'] += 1
+        print('Incorrect. The capital of {} is {}'.format(obj['name'], obj['capital']))
+    print('You have answered correctly for {} {} times'.format(obj['name'], obj['correct']))
+
+
+def loop_states():
+    count = 0
+    while count < len(states):
+        state = get_state_obj(count)
+        print('{})'.format(count+1))
+        guess = get_input(state)
+        check_answer(state, guess)
+        count += 1
+
+def intro_shuffle():
+    print("Welcome to the United States Capital Quiz \n Type the state capital of each state when you are prompted \n Check your score for each state, and practice to get better!")
+    random.shuffle(states)
+
+def check_play():
+    answer = input('Do you want to play again? y/n  ')
+    if answer.lower() == 'y' or answer.lower() == 'yes':
+        play_game()
+    elif answer.lower() == 'n' or answer.lower() == 'no':
+        print('Thanks for playing!')
+    else:
+        print("Sorry, I didn't get that")
+        check_play()
+
+def play_game():
+    intro_shuffle()
+    loop_states()
+    check_play()
+
+play_game()
+
+
+
